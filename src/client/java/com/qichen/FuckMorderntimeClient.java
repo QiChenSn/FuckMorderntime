1ac486d97a90ff9fb7cba62dca9d71fee4d4932e
QiChen <2102733232@qq.com>
2025-08-06 10:24:10 +0800

    Initial commit

---
+++ src/client/java/com/qichen/FuckMorderntimeClient.java
@@ -0,0 +1,10 @@
+package com.qichen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FuckMorderntimeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
